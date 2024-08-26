4e40619e01f513ef8b590dba6c3464b5c490ab03
Alex <alex.jian@grenoble-inp.org>
2024-08-26 18:02:33 +0900

    CLEAN: Clean the code.

--- app/evaluation/eval_everything.py
+++ app/evaluation/eval_everything.py
@@ -1,11 +1,10 @@
-from src.controllers import ssh_everything
 from typing import List, Tuple
+
 from loguru import logger
+from src.controllers import ssh_everything
 
 
-def eval_everything(
-    tasks: List[Tuple[str]], file_name: str
-) -> List[float]:
+def eval_everything(tasks: List[Tuple[str, str]], file_name: str):
     """Evaluate every task and make an average"""
     for task in tasks:
         logger.info(f"Starting the evaluation for the task: {task[1]}")
@@ -13,7 +12,10 @@ def eval_everything(
         while successfull_attempts < 5:
             try:
                 output = ssh_everything(
-                    user_query_database=task[0], user_query_analyzer=task[1], path="./data/log", file_name=file_name
+                    user_query_database=task[0],
+                    user_query_analyzer=task[1],
+                    path="./data/log",
+                    file_name=file_name,
                 )
                 logger.info("Output received, trying to evaluate")
                 output_list = eval(output)
@@ -25,7 +27,9 @@ def eval_everything(
                     )
                     logger.info("Starting to write inside the evaluation")
                     with open("evaluation.txt", mode="a") as file:
-                        file.write(f"For the task {task[0]} on file {file_name} at the attempts: {successfull_attempts}, the output is:\n")
+                        file.write(
+                            f"For the task {task[0]} on file {file_name} at the attempts: {successfull_attempts}, the output is:\n"
+                        )
                         file.write(str(output_list))
                         file.write("\n")
                         file.flush()
@@ -34,7 +38,9 @@ def eval_everything(
                 logger.info("Task failed, trying once again...")
                 successfull_attempts += 1
                 with open("evaluation.txt", mode="a") as file:
-                    file.write(f"For the task {task[0]} on file {file_name} at the attempts: {successfull_attempts}, the output is:")
+                    file.write(
+                        f"For the task {task[0]} on file {file_name} at the attempts: {successfull_attempts}, the output is:"
+                    )
                     file.write("[Failed to generate the output]")
                     file.write("\n")
                     file.flush()
@@ -43,11 +49,20 @@ def eval_everything(
 if __name__ == "__main__":
     file_names = ["auth.log", "auth2.log", "auth3.log"]
 
-    tasks = [("Please store all usernames that failed to log in", "Please list all distinct usernames that failed to log in with their number of attempts"), 
-        ("Please store all usernames that successfully logged in", "Please list all distinct usernames that successfully logged in"),
-        ("Please store all usernames that have their publickey accepted", "Please list all distinct usernames that have their publickey accepted"),
+    tasks = [
+        (
+            "Please store all usernames that failed to log in",
+            "Please list all distinct usernames that failed to log in with their number of attempts",
+        ),
+        (
+            "Please store all usernames that successfully logged in",
+            "Please list all distinct usernames that successfully logged in",
+        ),
+        (
+            "Please store all usernames that have their publickey accepted",
+            "Please list all distinct usernames that have their publickey accepted",
+        ),
     ]
-
 
     for i, file in enumerate(file_names):
         logger.info(f"Starting the evaluation for the file: {file}")
@@ -55,15 +70,31 @@ if __name__ == "__main__":
         logger.info(f"Successfully evaluate for the file: {file}")
 
     # For the whitelist
-    tasks = [("Here is a whitelist of authorized users: ['RAIN', 'SHUKA']. Please store all usernames that successfully logged in without being authorized", "Here is a whitelist of authorized users: ['RAIN', 'SHUKA']. Please list all distinct usernames that successfully logged in without being authorized"),
-        ("Here is a whitelist of authorized users: ['YAMAMOTO', 'MORI', 'SAEKI']. Please store all usernames that successfully logged in without being authorized", "Here is a whitelist of authorized users: ['YAMAMOTO', 'MORI', 'SAEKI']. Please list all distinct usernames that successfully logged in without being authorized"),
-        ("Here is a whitelist of authorized users: ['SHIMURA', 'TAKASUGI', 'HASEGAWA', 'KAMUI', 'MURATA']. Please store all usernames that successfully logged in without being authorized", "Here is a whitelist of authorized users: ['SHIMURA', 'TAKASUGI', 'HASEGAWA', 'KAMUI', 'MURATA']. Please list all distinct usernames that successfully logged in without being authorized"),
+    tasks = [
+        (
+            "Here is a whitelist of authorized users: ['RAIN', 'SHUKA']. Please store all usernames that successfully logged in without being authorized",
+            "Here is a whitelist of authorized users: ['RAIN', 'SHUKA']. Please list all distinct usernames that successfully logged in without being authorized",
+        ),
+        (
+            "Here is a whitelist of authorized users: ['YAMAMOTO', 'MORI', 'SAEKI']. Please store all usernames that successfully logged in without being authorized",
+            "Here is a whitelist of authorized users: ['YAMAMOTO', 'MORI', 'SAEKI']. Please list all distinct usernames that successfully logged in without being authorized",
+        ),
+        (
+            "Here is a whitelist of authorized users: ['SHIMURA', 'TAKASUGI', 'HASEGAWA', 'KAMUI', 'MURATA']. Please store all usernames that successfully logged in without being authorized",
+            "Here is a whitelist of authorized users: ['SHIMURA', 'TAKASUGI', 'HASEGAWA', 'KAMUI', 'MURATA']. Please list all distinct usernames that successfully logged in without being authorized",
+        ),
     ]
 
     for file, task in zip(file_names, tasks):
         logger.info(f"Starting the evaluation for the file {file} and the task {task}")
         eval_everything([task], file_name=file)
-        logger.info(f"Starting the evaluation for the file {file} and the task {task}")
-    eval_everything([("Please store all usernames that have their password accepted", "Please list all distinct usernames that have their password accepted")], file_name="auth2.log")
-        
-
+        logger.info(f"Successfully evaluate for the file {file}")
+    eval_everything(
+        [
+            (
+                "Please store all usernames that have their password accepted",
+                "Please list all distinct usernames that have their password accepted",
+            )
+        ],
+        file_name="auth2.log",
+    )
--- app/src/evaluation/embedding_finetuning_eval.py
+++
@@ -1,3 +0,0 @@
-from sentence_transformers.evaluation import InformationRetrievalEvaluator
-from sentence_transformers import SentenceTransformer
-from pathlib import Path
